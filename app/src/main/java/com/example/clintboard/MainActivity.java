ec7705fc461d333979cd7cd97014c50bdee0ce0c
goldhelixus <stealthchu@gmail.com>
2020-11-12 01:57:42 -0600

    clint_c button added

--- app/src/main/java/com/example/clintboard/MainActivity.java
+++ app/src/main/java/com/example/clintboard/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         ImageButton imageButtonlove = this.findViewById(R.id.imageButton_love);
         ImageButton imageButtonno = this.findViewById(R.id.imageButton_no);
         ImageButton imageButtono = this.findViewById(R.id.imageButton_o);
+        ImageButton imageButtonc = this.findViewById(R.id.imageButton_c);
 
         /*
             On Click Listeners that have nested On Completion Listener
@@ -61,6 +62,24 @@ public class MainActivity extends AppCompatActivity {
             the audio to the MediaPlayer and play. This makes it so we
             don't have to have a separate function to remount MediaPlayers.
         */
+        imageButtonc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (clint_sound != null && clint_sound.isPlaying()) {
+                    clint_sound.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mp) {
+                            clint_sound = MediaPlayer.create(MainActivity.this, R.raw.clint_c);
+                            clint_sound.start();
+                        }
+                    });
+                } else {
+                    clint_sound = MediaPlayer.create(MainActivity.this, R.raw.clint_c);
+                    clint_sound.start();
+
+                }
+            }
+        });
         imageButtono.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
